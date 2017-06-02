be91ff341518eaee0038c130f5fcebadf54f75d2
yuval.melamed <yuval.melamed@7450-171286.imperva.local>
2017-06-02 22:38:38 +0300

    Java ex14q1c~

--- src/GenericPerson.java
+++ src/GenericPerson.java
@@ -1,10 +1,11 @@
-
-import java.util.Iterator;
-
 /*
  * Maman 14 course 20554 question 2 by Yuval Melamed, ID 035870864
  */
+
+import java.util.Iterator;
+
 /**
+ * Program that demonstrates usage of generic sorted group of persons
  *
  * @author yuval.melamed
  */
@@ -14,26 +15,21 @@ public class GenericPerson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SortedGroup<Integer> sortedGroup = new SortedGroup<>();
-        
-        sortedGroup.add(32);
-        sortedGroup.add(2);
-        sortedGroup.add(8);
-        sortedGroup.add(3);
-        sortedGroup.add(17);
-        sortedGroup.add(8);
-        sortedGroup.add(81);
-        sortedGroup.add(19);
-        sortedGroup.add(21);
-        sortedGroup.add(8);
-        sortedGroup.add(1);
-        
+        SortedGroup<Person> sortedGroup = new SortedGroup<>();
+
+        sortedGroup.add(new Person("Jack", 123, 1937));
+        sortedGroup.add(new Person("Marlon", 234, 1924));
+        sortedGroup.add(new Person("Robert", 345, 1943));
+        sortedGroup.add(new Person("Al", 456, 1940));
+        sortedGroup.add(new Person("Ran", 567, 1980));
+        sortedGroup.add(new Person("Dan", 678, 1980));
+        sortedGroup.add(new Person("John", 789, 1970));
+
         System.out.println(sortedGroup);
-        int removeItem = 8;
-        System.out.println("sortedGroup.remove(" + removeItem + ") = " + sortedGroup.remove(removeItem));
-        for (Iterator<Integer> iter = sortedGroup.iterator(); iter.hasNext();) {
+        System.out.println("sortedGroup.remove(new Person(\"Anybody\", 123456789, 1980)) = " + sortedGroup.remove(new Person("Anybody", 123456789, 1980)));
+        for (Iterator<Person> iter = sortedGroup.iterator(); iter.hasNext();) {
             System.out.print(iter.next() + (iter.hasNext() ? ", " : "\n"));
         }
-        System.out.println("GenericUtil.reduce(sortedGroup, 21) = " + GenericUtil.reduce(sortedGroup, 21));
+        System.out.println("GenericUtil.reduce(sortedGroup, new Person(\"Anybody\", 123456789, 1980)) = " + GenericUtil.reduce(sortedGroup, new Person("Anybody", 123456789, 1980)));
     }
 }
