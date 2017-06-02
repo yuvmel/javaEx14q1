50b431c4dbd6c8a08d0c38e6ad919660fdd6f26a
yuval.melamed <yuval.melamed@7450-171286.imperva.local>
2017-06-02 23:07:00 +0300

    Java ex14q1c

--- src/GenericPerson.java
+++ src/GenericPerson.java
@@ -15,21 +15,41 @@ public class GenericPerson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        // Create new collection
         SortedGroup<Person> sortedGroup = new SortedGroup<>();
 
+        // Populate the collection
         sortedGroup.add(new Person("Jack", 123, 1937));
         sortedGroup.add(new Person("Marlon", 234, 1924));
         sortedGroup.add(new Person("Robert", 345, 1943));
         sortedGroup.add(new Person("Al", 456, 1940));
         sortedGroup.add(new Person("Ran", 567, 1980));
         sortedGroup.add(new Person("Dan", 678, 1980));
-        sortedGroup.add(new Person("John", 789, 1970));
+        sortedGroup.add(new Person("Baby", 789, 2017));
 
+        System.out.println("Persons: name #idNum @birthYear");
         System.out.println(sortedGroup);
-        System.out.println("sortedGroup.remove(new Person(\"Anybody\", 123456789, 1980)) = " + sortedGroup.remove(new Person("Anybody", 123456789, 1980)));
+        System.out.println();
+
+        // Remove person(s)
+        int year = 1980;
+        Person remover = new Person("Remover", 0, year);
+        int count = sortedGroup.remove(remover);
+
+        System.out.printf(
+                "After removing %d persons with the same birth year as: %s%n",
+                count, remover);
+        System.out.println(sortedGroup);
+        System.out.println();
+
+        Person reducer = new Person("Reducer", 0, 2000);
+        System.out.printf("Reduced to persons born before: %s%n%s%n%n",
+                reducer, GenericUtil.reduce(sortedGroup, reducer));
+
+        System.out.println("After removal, before reduction, by iteration:");
         for (Iterator<Person> iter = sortedGroup.iterator(); iter.hasNext();) {
-            System.out.print(iter.next() + (iter.hasNext() ? ", " : "\n"));
+            System.out.println(iter.next());
         }
-        System.out.println("GenericUtil.reduce(sortedGroup, new Person(\"Anybody\", 123456789, 1980)) = " + GenericUtil.reduce(sortedGroup, new Person("Anybody", 123456789, 1980)));
     }
 }
